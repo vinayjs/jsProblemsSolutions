b8a32eb8a66fa5dd5f09727789ae870c939b12e0
vinay chandran <vinay.sarvodaya1991@gmail.com>
2023-10-03 18:44:01 +0530

    check whether string is plaindrome or not ?

--- problem1.js
+++ problem1.js
@@ -24,4 +24,19 @@ const vowelsAndConsonants = (s) =>  {
     consonantChar.forEach((y)=> console.log(y))
 }
 
-vowelsAndConsonants('bcqaei')
+// vowelsAndConsonants('bcqaei')
+
+
+
+// Question- 2
+
+// check whether the given string is palindrome or not?
+
+
+const isPalindrome = (str) => {
+    const output = str.split('').reverse().join('')
+    return output === str ? 'Number is Plaindrome' : 'Number is not palindrome'
+}
+
+console.log(isPalindrome('aba'))
+console.log(isPalindrome('abc'))
